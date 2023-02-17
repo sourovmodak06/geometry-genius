e202cf11c5fb1ba768a6b159de6a70d9d69e2eb0
sourovmodak06 <sourovmodak650@gmail.com>
2023-02-18 00:59:57 +0600

    Random Color generator

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ function blogPage(){
 function homePage(){
     location.href = 'index.html';
 }
+
+// Random Color generator
+function getColor(elementId){
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = '#' + randomNumber.toString(16);
+    document.getElementById(elementId).style.backgroundColor = randomCode;
+}
