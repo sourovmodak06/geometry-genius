f9a416f20013bf710bb9836466a9931a1e32f68b
sourovmodak06 <sourovmodak650@gmail.com>
2023-02-18 11:11:16 +0600

    validation

--- js/script.js
+++ js/script.js
@@ -20,38 +20,65 @@ function calculate(firstValue,secondValue){
     return newValue;
 }
 
+// validation
+function validation(element,elementId){
+    document.getElementById(elementId).innerText = element.value;
+        if(isNaN(element.value)){
+            document.getElementById(elementId).innerText = 'Please enter number only❌';
+        }
+        else{
+            document.getElementById(elementId).innerText = '';
+            if (element.value.length === 0) {
+                    document.getElementById(elementId).innerText = 'Please enter number❎';
+            }
+            else if(parseFloat(element.value) <= 0){
+                document.getElementById(elementId).innerText = 'Please enter positive number❌';
+            }
+            else{
+                document.getElementById(elementId).innerText = '';
+            }
+        }
+}
+
+
 // triangle
 document.getElementById('triangle-calculate').addEventListener('click',function(){
     const triangleArea = 0.5 * calculate('triangle-base','triangle-height');
-    // console.log(triangleArea);
+    const triangleAreaFixed = triangleArea.toFixed(2);
+    console.log(triangleAreaFixed);
 });
 
 // Rectangle
 document.getElementById('rectangle-calculate').addEventListener('click',function(){
     const rectangleArea = calculate('rectangle-width','rectangle-length');
-    // console.log(rectangleArea);
+    const rectangleAreaFixed = rectangleArea.toFixed(2);
+    console.log(rectangleAreaFixed);
 });
 
 // Parallelogram
 document.getElementById('parallelogram-calculate').addEventListener('click',function(){
     const parallelogramArea = calculate('parallelogram-base','parallelogram-height');
-    console.log(parallelogramArea);
+    const parallelogramAreaFixed = parallelogramArea.toFixed(2);
+    console.log(parallelogramAreaFixed);
 });
 
 // Rhombus
 document.getElementById('rhombus-calculate').addEventListener('click',function(){
     const rhombusArea = 0.5 * calculate('rhombus-diagonal-one','rhombus-diagonal-two');
-    console.log(rhombusArea);
+    const rhombusAreaFixed = rhombusArea.toFixed(2);
+    console.log(rhombusAreaFixed);
 });
 
 // Pentagon
 document.getElementById('pentagon-calculate').addEventListener('click',function(){
     const pentagonArea = 0.5 * calculate('pentagon-first-filed','pentagon-second-filed');
-    console.log(pentagonArea);
+    const pentagonAreaFixed = pentagonArea.toFixed(2);
+    console.log(pentagonAreaFixed);
 });
 
 // Ellipse
 document.getElementById('ellipse-calculate').addEventListener('click',function(){
     const ellipseArea = 3.1416 * calculate('ellipse-a-axis','ellipse-b-axis');
-    console.log(ellipseArea);
+    const ellipseAreaFixed = ellipseArea.toFixed(2);
+    console.log(ellipseAreaFixed);
 });
