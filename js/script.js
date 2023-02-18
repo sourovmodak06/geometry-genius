3c5183311b6e395efb65a5b1bac2e1ffb43a6630
sourovmodak06 <sourovmodak650@gmail.com>
2023-02-18 13:19:28 +0600

    Area Calculation

--- js/script.js
+++ js/script.js
@@ -39,46 +39,112 @@ function validation(element,elementId){
             }
         }
 }
+// Name 
+function nameOfArea(elementIdData){
+    const nameData = document.getElementById(elementIdData).innerText;
+    return nameData;
+}
 
+// Edit 
+function editValue(elementId) {
+    document.getElementById(elementId).style.display ='block';
+}
+// serial area calculation
+let serial = 0;
 
 // triangle
 document.getElementById('triangle-calculate').addEventListener('click',function(){
+    serial += 1;
     const triangleArea = 0.5 * calculate('triangle-base','triangle-height');
     const triangleAreaFixed = triangleArea.toFixed(2);
-    console.log(triangleAreaFixed);
+    
+    const name = nameOfArea('nameTriangle');
+    displayData(name,triangleArea);
+});
+document.getElementById('triangle-edit').addEventListener('click',function(){
+    editValue('triangle-show');
 });
 
 // Rectangle
 document.getElementById('rectangle-calculate').addEventListener('click',function(){
+    serial += 1;
     const rectangleArea = calculate('rectangle-width','rectangle-length');
     const rectangleAreaFixed = rectangleArea.toFixed(2);
-    console.log(rectangleAreaFixed);
+
+    const name = nameOfArea('nameRectangle');
+    displayData(name,rectangleAreaFixed);
+});
+document.getElementById('rectangle-edit').addEventListener('click',function(){
+    editValue('rectangle-show');
 });
 
 // Parallelogram
 document.getElementById('parallelogram-calculate').addEventListener('click',function(){
+    serial += 1;
     const parallelogramArea = calculate('parallelogram-base','parallelogram-height');
     const parallelogramAreaFixed = parallelogramArea.toFixed(2);
-    console.log(parallelogramAreaFixed);
+    
+    const name = nameOfArea('nameParallelogram');
+    displayData(name,parallelogramAreaFixed);
+});
+document.getElementById('parallelogram-edit').addEventListener('click',function(){
+    editValue('parallelogram-show');
 });
 
 // Rhombus
 document.getElementById('rhombus-calculate').addEventListener('click',function(){
+    serial += 1;
     const rhombusArea = 0.5 * calculate('rhombus-diagonal-one','rhombus-diagonal-two');
     const rhombusAreaFixed = rhombusArea.toFixed(2);
-    console.log(rhombusAreaFixed);
+    
+    const name = nameOfArea('nameRhombus');
+    displayData(name,rhombusAreaFixed);
+});
+document.getElementById('rhombus-edit').addEventListener('click',function(){
+    editValue('rhombus-show');
 });
 
 // Pentagon
 document.getElementById('pentagon-calculate').addEventListener('click',function(){
+    serial += 1;
     const pentagonArea = 0.5 * calculate('pentagon-first-filed','pentagon-second-filed');
     const pentagonAreaFixed = pentagonArea.toFixed(2);
-    console.log(pentagonAreaFixed);
+    
+    const name = nameOfArea('namePentagon');
+    displayData(name,pentagonAreaFixed);
+});
+document.getElementById('pentagon-edit').addEventListener('click',function(){
+    editValue('pentagon-show');
 });
 
 // Ellipse
 document.getElementById('ellipse-calculate').addEventListener('click',function(){
+    serial += 1;
     const ellipseArea = 3.1416 * calculate('ellipse-a-axis','ellipse-b-axis');
     const ellipseAreaFixed = ellipseArea.toFixed(2);
-    console.log(ellipseAreaFixed);
+    
+    const name = nameOfArea('nameEllipse');
+    displayData(name,ellipseAreaFixed);
 });
+document.getElementById('ellipse-edit').addEventListener('click',function(){
+    editValue('ellipse-show');
+});
+
+
+// Area Calculation
+function displayData(areaName,area) {
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.style.display = 'flex';
+    tr.style.alignItems = 'center';
+    tr.style.margin ='16px 0';
+
+    tr.innerHTML = `
+      <td class="mr-4">${serial}</td>
+      <td>${areaName}</td>
+      <td class="ml-2">${area}cm<sup>2</sup></td>
+      <td class="text-white bg-blue-600 px-3 rounded-lg ml-4">Covert to m<sup>2</sup></td>      
+    `;
+    container.appendChild(tr);
+    document.getElementById("total-product").innerText = serial;
+}
